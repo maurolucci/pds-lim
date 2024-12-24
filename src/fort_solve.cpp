4cbc2a9680cee4d7d4e8306efd8273c2c230bca5
Mauro Lucci <maurolucci.14@gmail.com>
2024-12-24 00:41:54 -0300

    Fix: optimization of monitored set calculation

--- src/fort_solve.cpp
+++ src/fort_solve.cpp
@@ -137,50 +137,63 @@ private:
 
     // Minimize feasible solution
     for (Vertex v : unmonitoredSet) {
-      VertexList mS(num_vertices(graph), true);
 
       if (forts.size() >= fortsLimit)
         break;
 
       sValue.at(v) = 0.0;
 
-      // Recompute monitored set
-      std::list<Vertex> changed;
-      changed.push_back(v);
+      // Can be easily fixed?
+      bool fixed = false;
       for (auto u : boost::make_iterator_range(adjacent_vertices(v, graph)))
-        changed.push_back(u);
-      while (!changed.empty()) {
-        Vertex u = changed.front();
-        changed.pop_front();
-        if (sValue.at(u) > 0.5) // u has a pmu -> it's still monitored
-          continue;
-        bool isMonitored = false;
-        for (auto z : boost::make_iterator_range(adjacent_vertices(u, graph)))
-          if (sValue.at(z) > 0.5 && wValue.at(std::make_pair(z, u)) > 0.5) {
-            isMonitored = true;
-            break;
-          }
-        if (isMonitored) // u is monitored by z with dom. rule
-          continue;
-        for (auto z : boost::make_iterator_range(adjacent_vertices(u, graph))) {
-          if (!input.isZeroInjection(z) || !mS[z])
-            continue;
-          size_t count = boost::range::count_if(
-              boost::adjacent_vertices(z, graph),
-              [mS, u](auto y) { return y != u && mS[y]; });
-          if (boost::degree(z, graph) - count != 1)
-            continue;
-          isMonitored = true;
+        if (sValue.at(u) > 0.5 && wValue.at(std::make_pair(u, v)) > 0.5) {
+          fixed = true;
           break;
         }
-        if (isMonitored)
-          continue;
-        if (!mS[u])
-          continue;
-        mS[u] = false;
-        for (auto z : boost::make_iterator_range(adjacent_vertices(u, graph)))
-          changed.push_back(z);
+
+      if (fixed)
+        continue;
+
+      VertexList mS = input.get_monitored_set(sValue, wValue);
+      if (!input.isFeasible(mS)) {
+        forts.insert(findFort(mS));
+        sValue.at(v) = 1.0;
       }
+
+      /*
+            // Recompute monitored set
+            std::list<Vertex> changed;
+            changed.push_back(v);
+            for (auto u : boost::make_iterator_range(adjacent_vertices(v,
+         graph))) changed.push_back(u); while (!changed.empty()) { Vertex u
+         = changed.front(); changed.pop_front(); if (sValue.at(u) > 0.5) //
+         u has a pmu -> it's still monitored continue; bool isMonitored =
+         false; for (auto z :
+         boost::make_iterator_range(adjacent_vertices(u, graph))) if
+         (sValue.at(z) > 0.5 && wValue.at(std::make_pair(z, u)) > 0.5) {
+                  isMonitored = true;
+                  break;
+                }
+              if (isMonitored) // u is monitored by z with dom. rule
+                continue;
+              for (auto z : boost::make_iterator_range(adjacent_vertices(u,
+         graph))) { if (!input.isZeroInjection(z) || !mS[z]) continue;
+                size_t count = boost::range::count_if(
+                    boost::adjacent_vertices(z, graph),
+                    [mS, u](auto y) { return y != u && mS[y]; });
+                if (boost::degree(z, graph) - count != 1)
+                  continue;
+                isMonitored = true;
+                break;
+              }
+              if (isMonitored)
+                continue;
+              if (!mS[u])
+                continue;
+              mS[u] = false;
+              for (auto z : boost::make_iterator_range(adjacent_vertices(u,
+         graph))) changed.push_back(z);
+            }
 
       VertexList mS2 = input.get_monitored_set(sValue, wValue);
       assert(mS == mS2);
@@ -188,6 +201,7 @@ private:
         forts.insert(findFort(mS2));
         sValue.at(v) = 1.0;
       }
+      */
     }
 
     return forts;
