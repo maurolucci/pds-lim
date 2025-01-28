b08b0607b987e53f7ddcf7153a2673f9bf906969
maurolucci <maurolucci.14@gmail.com>
2025-01-28 19:28:18 -0300

    Add: script for experiment #2

---
+++ experiments/script2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+TIMEOUT=900
+NRUN=5
+MAXNODES=1000
+declare -a SOLVERS=("brimkov" "jovanovic" "forts" "cycles")
+CASES="instances.csv"
+BIN="../pds-lim"
+INPUT="../inputs/"
+OUTPUT="../outputs/exp2/"
+
+mkdir -p $OUTPUT 
+
+tail -n +2 $CASES | while read -r line 
+do
+    IFS=',' read -r index name vertices degree <<< $line
+    if [ $vertices -gt $MAXNODES ]
+    then
+        continue
+    fi
+    # solve
+    for solver in "${SOLVERS[@]}"
+    do
+        for omega in $(seq 1 $(($degree+1)))
+        do
+            date
+            time $BIN -s $solver -w $omega -f $INPUT$name -z -n $NRUN -t $TIMEOUT -o $OUTPUT
+        done
+    done
+done
