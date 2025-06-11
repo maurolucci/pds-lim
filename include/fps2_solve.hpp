9e1a9bccba9cee180b8e2f01b5c3ecef8da3c4db
Mauro Lucci <maurolucci.14@gmail.com>
2025-06-11 19:07:48 -0300

    Add: valid inequalities for INF-FPS

--- include/fps2_solve.hpp
+++ include/fps2_solve.hpp
@@ -6,7 +6,7 @@
 
 namespace pds {
 SolveResult solveLazyFpss2(Pds &, boost::optional<std::string>, std::ostream &,
-                           std::ostream &, double, size_t size_t);
+                           std::ostream &, double, size_t, size_t);
 } // end of namespace pds
 
 #endif // FPS2_SOLVE_HPP
