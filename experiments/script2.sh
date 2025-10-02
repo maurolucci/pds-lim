3edaf8cf5c07739d845bc532bbbdbead36b2417a
Mauro Lucci <maurolucci.14@gmail.com>
2025-10-02 12:18:00 -0300

    Add experiments for cuts

--- experiments/script2.sh
+++ experiments/script2.sh
@@ -5,6 +5,8 @@ TIMEOUT=900
 NRUN=5
 MAXNODES=15000
 declare -a SOLVERS=("efpss")
+declare -a CUTMAX=("5" "10" "25")
+declare -a CUTFREQ=("100" "1000" "10000")
 CASES="instances.csv"
 BIN="../pds-lim"
 INPUT="../inputs/"
@@ -30,6 +32,13 @@ do
             time $BIN -s $solver -w $omega -f $INPUT$name -n $NRUN -t $TIMEOUT -o $OUTPUT --in-prop --out-prop
             time $BIN -s $solver -w $omega -f $INPUT$name -n $NRUN -t $TIMEOUT -o $OUTPUT --out-prop --init-efps
             time $BIN -s $solver -w $omega -f $INPUT$name -n $NRUN -t $TIMEOUT -o $OUTPUT --in-prop --out-prop --init-efps
+            for cutmax in "${CUTMAX[@]}"
+            do
+                for cutfreq in "${CUTFREQ[@]}"
+                do
+                    time $BIN -s $solver -w $omega -f $INPUT$name -n $NRUN -t $TIMEOUT -o $OUTPUT --out-prop --init-efps --cuts --cut-max $cutmax --cut-freq $cutfreq
+                done
+            done
         done
     done
 done
